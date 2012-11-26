4f571e15db2ea7f694ac4e4f4fb4ed4d9e7c5ed1
Fraser Rackham <frackham@googlemail.com>
2012-11-26 22:22:00 +0000

    Added precommit and predeploy placeholders.
    Updated ignore file too.

---
+++ _dev-process/precommit.py
@@ -0,0 +1,6 @@
+#HACK: Exists because Github for Windows can't execute precommit hooks :/ .
+#TODO: [c] Make executable.
+#TODO: [e] Run codeanalysis.
+#TODO: [i] Probably move codeanalysis in here.
+# *Don't* make a backup using this. Point of the VCS is to manage the history...
+print("Precommit")
---
+++ _dev-process/predeploy.py
@@ -0,0 +1,6 @@
+#TODO: [c] Make executable.
+#TODO: [e] Load test strut and check unit tests.
+#TODO: [e] Run code analysis.
+#TODO: [i] Make change to app.yaml?
+#TODO: [e] Make local backup.
+print("Predeploy")
